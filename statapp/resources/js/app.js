4642c58326cfe3c64b4fbe5a92d9d0a6c368af06
fg607 <gangf333@gmail.com>
2020-02-21 11:24:36 +0800

    add vue-echarts

--- statapp/resources/js/app.js
+++ statapp/resources/js/app.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import ECharts from 'vue-echarts/components/ECharts.vue'
+
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/polar'
+import 'echarts/lib/component/legend'
+import 'echarts/lib/component/title.js'
 
 Vue.use(VueRouter)
+Vue.component('v-chart', ECharts)
 
 import App from './views/App'
 import Hello from './views/Hello'
 import Home from './views/Home'
+import Echarts from './views/Echarts'
 
 const router = new VueRouter({
     mode: 'history',
@@ -19,6 +28,11 @@ const router = new VueRouter({
             path: '/hello',
             name: 'hello',
             component: Hello,
+        },
+        {
+            path: '/echarts',
+            name: 'echart',
+            component: Echarts,
         },
     ],
 });
