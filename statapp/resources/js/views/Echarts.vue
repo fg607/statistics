aab4e4d48483adfe8d9c013b5c4c86402e960b25
fg607 <gangf333@gmail.com>
2020-02-21 15:27:17 +0800

    update App.vue

--- statapp/resources/js/views/Echarts.vue
+++ statapp/resources/js/views/Echarts.vue
@@ -1,61 +1,71 @@
+<template>
+    <v-chart :options="polar"/>
+</template>
+
 <style>
+    /**
+     * The default size is 600px×400px, for responsive charts
+     * you may need to set percentage values as follows (also
+     * don't forget to provide a size for the container).
+     */
     .echarts {
         width: 100%;
         height: 100%;
     }
 </style>
 
-<template>
-    <v-chart theme="ovilia-green" :options="polar"/>
-</template>
-
 <script>
     import ECharts from 'vue-echarts'
-    import echarts from 'echarts'
+    import 'echarts/lib/chart/line'
+    import 'echarts/lib/component/polar'
+
     export default {
         components: {
             'v-chart': ECharts
         },
         data () {
+            let data = []
+
+            for (let i = 0; i <= 360; i++) {
+                let t = i / 180 * Math.PI
+                let r = Math.sin(2 * t) * Math.cos(2 * t)
+                data.push([r, i])
+            }
+
             return {
                 polar: {
-                    title : {
-                        text: '会员数据统计',
-                        subtext: '动态数据',
-                        x:'center'
-                    },
-                    tooltip : {
-                        trigger: 'item',
-                        formatter: "{a} <br/>{b} : {c} ({d}%)"
+                    title: {
+                        text: '极坐标双数值轴'
                     },
                     legend: {
-                        show: true,
-                        orient: 'vertical',
-                        left: 'left',
-                        data: ['微信访问','公众号访问','扫码进入','分享进入','搜索访问']
+                        data: ['line']
                     },
-                    series : [
-                        {
-                            name: '访问来源',
-                            type: 'pie',
-                            radius : '55%',
-                            center: ['50%', '60%'],
-                            data:[
-                                {value:335, name:'微信访问'},
-                                {value:310, name:'公众号访问'},
-                                {value:234, name:'扫码进入'},
-                                {value:135, name:'分享进入'},
-                                {value:1548, name:'搜索访问'}
-                            ],
-                            itemStyle: {
-                                emphasis: {
-                                    shadowBlur: 10,
-                                    shadowOffsetX: 0,
-                                    shadowColor: 'rgba(0, 0, 0, 0.5)'
-                                }
-                            }
+                    polar: {
+                        center: ['50%', '54%']
+                    },
+                    tooltip: {
+                        trigger: 'axis',
+                        axisPointer: {
+                            type: 'cross'
                         }
-                    ]
+                    },
+                    angleAxis: {
+                        type: 'value',
+                        startAngle: 0
+                    },
+                    radiusAxis: {
+                        min: 0
+                    },
+                    series: [
+                        {
+                            coordinateSystem: 'polar',
+                            name: 'line',
+                            type: 'line',
+                            showSymbol: false,
+                            data: data
+                        }
+                    ],
+                    animationDuration: 2000
                 }
             }
         }
