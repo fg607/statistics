014b250566e3e96bf9693f640a0379e784e3d6e7
fg607 <gangf333@gmail.com>
2020-02-21 11:57:04 +0800

    update app.js

--- statapp/resources/js/app.js
+++ statapp/resources/js/app.js
@@ -2,12 +2,11 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import ECharts from 'vue-echarts/components/ECharts.vue'
 
-import 'echarts/lib/chart/bar'
+/*import 'echarts/lib/chart/bar'
 import 'echarts/lib/component/tooltip'
 import 'echarts/lib/component/polar'
 import 'echarts/lib/component/legend'
-import 'echarts/lib/component/title.js'
-import 'echarts/lib/chart/pie'
+import 'echarts/lib/component/title.js'*/
 
 Vue.use(VueRouter)
 Vue.component('v-chart', ECharts)
