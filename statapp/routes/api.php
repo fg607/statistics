55b166f52ab45701301a0b7b54e8f17130278258
fg607 <gangf333@gmail.com>
2020-02-22 22:40:47 +0800

    update PeopleInfoController

--- statapp/routes/api.php
+++ statapp/routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/pepole','PeopleInfoController@getHubei')
+Route::get('/pepoleHubei','PeopleInfoController@getHubei')
