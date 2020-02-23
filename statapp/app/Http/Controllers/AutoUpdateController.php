4c9901a8cccb8b4648bd5366b1bc710d87c0d858
fg607 <gangf333@gmail.com>
2020-02-24 00:56:44 +0800

    update controller

--- statapp/app/Http/Controllers/AutoUpdateController.php
+++ statapp/app/Http/Controllers/AutoUpdateController.php
@@ -11,27 +11,29 @@ class AutoUpdateController extends Controller
 {
     public function updateDayliyIsolate(){
 
+        date_default_timezone_set("PRC");
+
         $peopleInfos = BackPeopleInfo::where('isolate_date','<=',date("Y-m-d", strtotime("-2 week")))->get();
 
-        foreach($peopleInfos as $peopleInfo){
+//        foreach($peopleInfos as $peopleInfo){
+//
+//            if($peopleInfo->isolate_flag == '否'){
+//
+//                $peoleInfo->isolate_flag = '是';
+//                $peopleInfo->save();
+//            }
+//        }
+//
+//        $isolate = BackPeopleInfo::where('isolate_flag','否')->get();
+//
+//        $dayliyIsolate = new DayliyIsolateNumber;
+//
+//        $dayliyIsolate->date = date("Y-m-d");
+//        $dayliyIsolate->numbers = count($isolate);
+//
+//        $dayliyIsolate->save();
 
-            if($peopleInfo->isolate_flag == '否'){
-
-                $peoleInfo->isolate_flag = '是';
-                $peopleInfo->save();
-            }
-        }
-
-        $isolate = BackPeopleInfo::where('isolate_flag','否')->get();
-
-        $dayliyIsolate = new DayliyIsolateNumber;
-
-        $dayliyIsolate->date = date("Y-m-d");
-        $dayliyIsolate->numbers = count($isolate);
-
-        $dayliyIsolate->save();
-
-       // return count($isolate);
+        return $peopleInfos;
 
     }
 }
