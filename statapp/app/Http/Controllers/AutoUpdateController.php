368e2945bbba6e7e66a7581f0e9b88a0ff5d9ed0
fg607 <gangf333@gmail.com>
2020-02-23 12:20:36 +0800

    update Controller

--- statapp/app/Http/Controllers/AutoUpdateController.php
+++ statapp/app/Http/Controllers/AutoUpdateController.php
@@ -11,9 +11,21 @@ class AutoUpdateController extends Controller
 {
     public function updateDayliyIsolate(){
 
-        $peopleInfos = BackPeopleInfo::where('isolate_date','<=',date("Y-m-d", strtotime("-2 week")))->get();
+        //$peopleInfos = BackPeopleInfo::where('isolate_date','<=',date("Y-m-d", strtotime("-2 week")))->get();
 
-        return $peopleInfos;
+        /*foreach($peopleInfos as $peopleInfo){
+
+            if($peopleInfo->isolate_flag == '否'){
+
+                $peoleInfo->isolate_flag = '是';
+                $peopleInfo->save();
+            }
+        }
+
+        $isolate = BackPeopleInfo::where('isolate_flag','否')->get();*/
+
+       // return count($isolate);
+        return date("Y-m-d", strtotime("-2 week"));
 
     }
 }
