27c47cf242821dc4b534980e60b1efc08b52ca60
fg607 <gangf333@gmail.com>
2020-02-23 12:04:38 +0800

    update Controller

--- statapp/app/Http/Controllers/AutoUpdateController.php
+++ statapp/app/Http/Controllers/AutoUpdateController.php
@@ -11,7 +11,7 @@ class AutoUpdateController extends Controller
 {
     public function updateDayliyIsolate(){
 
-        $peopleInfos = BackPeopleInfo::where('isolate_date','>',date(‘Y-m-d’,strtotime(‘-2 week’)))->get();
+        $peopleInfos = BackPeopleInfo::where('isolate_date','>',date("Y-m-d", strtotime("-1 week")))->get();
 
         return count($peopleInfos);
 
