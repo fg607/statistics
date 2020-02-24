43daf737a1e1a68c9d1eee06ed6efab2dbb6d64e
fg607 <gangf333@gmail.com>
2020-02-25 00:14:09 +0800

    update controller

--- statapp/app/Http/Controllers/StatisticsController.php
+++ statapp/app/Http/Controllers/StatisticsController.php
@@ -17,34 +17,30 @@ class StatisticsController extends Controller
 
     public function getStatistics(){
 
-         $hotRooms = BackPeopleInfo::where('isolate_level','<>','普通')->select('address')->distinct()->count();
-         $hotPeople = BackPeopleInfo::where('isolate_level','<>','普通')->count();
-         $hotIsolateRooms = BackPeopleInfo::where([['isolate_level','<>','普通'],['isolate_flag','否']])->select('address')->distinct()->count();
-         $hotIsolatePeople = BackPeopleInfo::where([['isolate_level','<>','普通'],['isolate_flag','否']])->count();
-         $hubeiIsolateRooms = BackPeopleInfo::where([['isolate_level','like','%湖北%'],['isolate_flag','否']])->select('address')->distinct()->count();
-         $hubeiIsolatePeople = BackPeopleInfo::where([['isolate_level','like','%湖北%'],['isolate_flag','否']])->count();
-         $otherRooms = BackPeopleInfo::where('isolate_level','普通')->select('address')->distinct()->count();
-         $otherPeople = BackPeopleInfo::where('isolate_level','普通')->count();
-         $otherIsolateRooms = BackPeopleInfo::where([['isolate_level','普通'],['isolate_flag','否']])->select('address')->distinct()->count();
-         $otherIsolatePeople = BackPeopleInfo::where([['isolate_level','普通'],['isolate_flag','否']])->count();
-         $freeOtherRooms = $otherRooms - $otherIsolateRooms;
-         $freeOtherPeople = $otherPeople - $otherIsolatePeople;
-         $qrcodeRooms = BackPeopleInfo::where('qrcode_flag','是')->select('address')->distinct()->count();
-         $qrcodePeople = BackPeopleInfo::where('qrcode_flag','是')->count();
-         $qrcodeIsolateRooms = BackPeopleInfo::where([['qrcode_flag','是'],['isolate_flag','否']])->select('address')->distinct()->count();
-         $qrcodeIsolatePeople = BackPeopleInfo::where([['qrcode_flag','是'],['isolate_flag','否']])->count();
-         $qrcodeFreeRooms = $qrcodeRooms - $qrcodeIsolateRooms;
-         $qrcodeFreePeople = $qrcodePeople - $qrcodeIsolatePeople;
-
-         return array('hotRooms'=>$hotRooms,'hotPeople'=>$hotPeople,'hotIsolateRooms'=>$hotIsolateRooms,'hotIsolatePeople'=>$hotIsolatePeople,
-                 'hubeiIsolateRooms'=>$hubeiIsolateRooms,'hubeiIsolatePeople'=>$hubeiIsolatePeople,'otherRooms'=>$otherRooms,
-                  'otherPeople'=>otherPeople,'otherIsolateRooms'=>$otherIsolateRooms,'otherIsolatePeople'=>$otherIsolatePeople,
-                  'freeOtherRooms'=>$freeOtherRooms,'freeOtherPeople'=>$freeOtherPeople,'qrcodeRooms'=>$qrcodeRooms,'qrcodePeople'=>$qrcodePeople,
-                  'qrcodeIsolateRooms'=>$qrcodeIsolateRooms,'qrcodeIsolatePeople'=>$qrcodeIsolatePeople,'qrcodeFreeRooms'=>$qrcodeFreeRooms,
-                  'qrcodeFreePeople'=>$qrcodeFreePeople);
-
-
-
-
+//         $hotRooms = BackPeopleInfo::where('isolate_level','<>','普通')->select('address')->distinct()->count();
+//         $hotPeople = BackPeopleInfo::where('isolate_level','<>','普通')->count();
+//         $hotIsolateRooms = BackPeopleInfo::where([['isolate_level','<>','普通'],['isolate_flag','否']])->select('address')->distinct()->count();
+//         $hotIsolatePeople = BackPeopleInfo::where([['isolate_level','<>','普通'],['isolate_flag','否']])->count();
+//         $hubeiIsolateRooms = BackPeopleInfo::where([['isolate_level','like','%湖北%'],['isolate_flag','否']])->select('address')->distinct()->count();
+//         $hubeiIsolatePeople = BackPeopleInfo::where([['isolate_level','like','%湖北%'],['isolate_flag','否']])->count();
+//         $otherRooms = BackPeopleInfo::where('isolate_level','普通')->select('address')->distinct()->count();
+//         $otherPeople = BackPeopleInfo::where('isolate_level','普通')->count();
+//         $otherIsolateRooms = BackPeopleInfo::where([['isolate_level','普通'],['isolate_flag','否']])->select('address')->distinct()->count();
+//         $otherIsolatePeople = BackPeopleInfo::where([['isolate_level','普通'],['isolate_flag','否']])->count();
+//         $freeOtherRooms = $otherRooms - $otherIsolateRooms;
+//         $freeOtherPeople = $otherPeople - $otherIsolatePeople;
+//         $qrcodeRooms = BackPeopleInfo::where('qrcode_flag','是')->select('address')->distinct()->count();
+//         $qrcodePeople = BackPeopleInfo::where('qrcode_flag','是')->count();
+//         $qrcodeIsolateRooms = BackPeopleInfo::where([['qrcode_flag','是'],['isolate_flag','否']])->select('address')->distinct()->count();
+//         $qrcodeIsolatePeople = BackPeopleInfo::where([['qrcode_flag','是'],['isolate_flag','否']])->count();
+//         $qrcodeFreeRooms = $qrcodeRooms - $qrcodeIsolateRooms;
+//         $qrcodeFreePeople = $qrcodePeople - $qrcodeIsolatePeople;
+//
+//         return array('hotRooms'=>$hotRooms,'hotPeople'=>$hotPeople,'hotIsolateRooms'=>$hotIsolateRooms,'hotIsolatePeople'=>$hotIsolatePeople,
+//                 'hubeiIsolateRooms'=>$hubeiIsolateRooms,'hubeiIsolatePeople'=>$hubeiIsolatePeople,'otherRooms'=>$otherRooms,
+//                  'otherPeople'=>otherPeople,'otherIsolateRooms'=>$otherIsolateRooms,'otherIsolatePeople'=>$otherIsolatePeople,
+//                  'freeOtherRooms'=>$freeOtherRooms,'freeOtherPeople'=>$freeOtherPeople,'qrcodeRooms'=>$qrcodeRooms,'qrcodePeople'=>$qrcodePeople,
+//                  'qrcodeIsolateRooms'=>$qrcodeIsolateRooms,'qrcodeIsolatePeople'=>$qrcodeIsolatePeople,'qrcodeFreeRooms'=>$qrcodeFreeRooms,
+//                  'qrcodeFreePeople'=>$qrcodeFreePeople);
     }
 }
