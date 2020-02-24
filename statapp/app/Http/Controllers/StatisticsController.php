c3031442ac165e66a7e8a2b30f5b3b529bfd9fd8
fg607 <gangf333@gmail.com>
2020-02-24 16:11:26 +0800

    add StatisticsController

--- statapp/app/Http/Controllers/StatisticsController.php
+++ statapp/app/Http/Controllers/StatisticsController.php
@@ -14,4 +14,18 @@ class StatisticsController extends Controller
 
         return count($rooms);
     }
+
+    public function getStatistics(){
+
+         $rooms = BackPeopleInfo::select('address')->distinct()->get();
+         $people = BackPeopleInfo::all();
+         $qrcodeRooms = BackPeopleInfo::where('qrcode_flag','是')->select('address')->distinct()->get();
+         $qrcodePeople = BackPeopleInfo::where('qrcode_flag','是')->get();
+
+         return array('rooms'=>count($rooms),'people'=>count($people),'qrcodeRooms'=>count($qrcodeRooms),'qrcodePeople'=>count($qrcodePeople));
+
+
+
+
+    }
 }
--- statapp/routes/api.php
+++ statapp/routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('/peopleinfos','PeopleInfoController@getPeopleInfos');
 Route::get('/dayliy','AutoUpdateController@updateDayliyIsolate');
-Route::get('/statistics','StatisticsController@getAllRooms');
+Route::get('/statistics','StatisticsController@getStatistics');
 
