7c657112256e3359ab986159c85699ab27685dbd
fg607 <gangf333@gmail.com>
2020-02-25 10:52:14 +0800

    update BackPeopleInfo

--- statapp/app/BackPeopleInfo.php
+++ statapp/app/BackPeopleInfo.php
@@ -3,11 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\DayliyIsolateNumber;
 
 class BackPeopleInfo extends Model
 {
     //
     public $timestamps = false;
+
+    public static function boot()
+    	{
+        	parent::boot();
+
+        	static::created(function ($model) {
+
+        	    if($model->isolate_flag == '否'){
+
+        	        $dayliyIsolate = DayliyIsolateNumber::where('date',date("Y-m-d"))->first();
+
+        	        $dayliyIsolate->numbers = $dayliyIsolate->numbers + 1;
+
+        	        $dayliyIsolate->save();
+
+        	    }
+
+        	});
+        }
 
 
 }
